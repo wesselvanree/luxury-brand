e8fdd1fb0c75e0272d092b22b18c64a020a53f39
Wessel van Ree <wesselvanree@gmail.com>
2020-07-21 10:30:17 +0200

    Fixed typo

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,64 +1,100 @@
 const smoothScrollSettings = {
   navigationBreakpoint: 800,
   distanceFromTopDesktop: 0,
-  distanceFromTopMobile: 20,
-  animationDuration: 800,
+  distanceFromTopMobile: 0,
+  animationDuration: 600,
+  changeUrl: false,
 };
 
-// setting event listeners for smooth scrolling
-smoothScrollLinks = document.querySelectorAll(".js-scroll");
+// setting event listeners for anchor tags with # in the href attribute
+const smoothScrollLinks = document.querySelectorAll("a[href*='#']");
 smoothScrollLinks.forEach(function (smoothScrollLink) {
   smoothScrollLink.addEventListener("click", smoothScroll);
+});
+
+// setting event listeners for elements with the js-scroll class
+const smoothScrollElements = document.querySelectorAll(".js-scroll");
+smoothScrollElements.forEach(function (smoothScrollElement) {
+  smoothScrollElement.addEventListener("click", smoothScroll);
 });
 
 // function for smooth scrolling
 function smoothScroll(event) {
   event.preventDefault();
-  const targetQuerySelector = event.currentTarget.getAttribute("href");
 
-  // the targetPosition depends on the screen size because of my different topnavs
+  let targetQuerySelector;
+  if (event.currentTarget.tagName == "A") {
+    targetQuerySelector = event.currentTarget.getAttribute("href");
+    if (smoothScrollSettings.changeUrl) {
+      history.replaceState(undefined, undefined, targetQuerySelector);
+    }
+  } else if (
+    (" " + event.currentTarget.className + " ").indexOf(".js-scroll" > -1) &&
+    event.currentTarget.dataset.target
+  ) {
+    targetQuerySelector = event.currentTarget.dataset.target;
+  } else {
+    targetQuerySelector = "#";
+    console.error(
+      "ERROR: No valid element provided, make sure you are using data-target='targetQuerySelector' to set the target element."
+    );
+  }
+
+  // the targetPosition depends on the screen width
   const screenWidth =
     window.innerWidth ||
     document.documentElement.clientWidth ||
     document.getElementsByTagName("body")[0].clientWidth;
 
-  // calculate how many px will need to be scrolled less because of the navigation bar
-  navigationHeight = 0;
+  // calculate how many px will need to be scrolled
   if (screenWidth > smoothScrollSettings.navigationBreakpoint) {
-    navigationHeight = smoothScrollSettings.distanceFromTopDesktop;
+    distanceFromTop = smoothScrollSettings.distanceFromTopDesktop;
   } else {
-    navigationHeight = smoothScrollSettings.distanceFromTopMobile;
+    distanceFromTop = smoothScrollSettings.distanceFromTopMobile;
   }
 
   // if targetQuerySelector = "#" --> scroll to top
   const targetPosition =
     targetQuerySelector == "#"
       ? 0
-      : document.querySelector(targetQuerySelector).offsetTop -
-        navigationHeight;
+      : document.querySelector(targetQuerySelector).offsetTop - distanceFromTop;
 
   const startPosition = window.pageYOffset;
   let distance = targetPosition - startPosition;
-  const duration = 800;
+  const duration = smoothScrollSettings.animationDuration;
   let start = null;
 
-  // animation
   window.requestAnimationFrame(step);
 
+  // abort scrolling when user scrolls
+  let previousPosition = null;
+  let abortAnimation = false;
+
   function step(timestamp) {
-    if (!start) start = timestamp;
-    const progress = timestamp - start;
-    window.scrollTo(
-      0,
-      timingFunction(
+    if (!start) {
+      start = timestamp;
+      previousPosition = window.pageYOffset;
+    }
+    if (previousPosition && parseInt(previousPosition) != window.pageYOffset) {
+      abortAnimation = true;
+    }
+
+    // if user didn't scroll
+    if (!abortAnimation) {
+      const progress = timestamp - start;
+      const newPos = timingFunction(
         progress,
         startPosition,
         distance,
         smoothScrollSettings.animationDuration
-      )
-    );
-    if (progress < duration) {
-      window.requestAnimationFrame(step);
+      );
+      window.scrollTo(0, newPos);
+      if (progress < duration) {
+        previousPosition = newPos;
+        window.requestAnimationFrame(step);
+      } else {
+        previousPosition = null;
+      }
     }
   }
 
